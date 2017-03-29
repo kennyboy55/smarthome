9d5217d459eae4e0d1ecc2ab4072617af9e5c9ee
Bluepr1nt <martijncaluwe@gmail.com>
2017-03-29 13:59:31 +0200

    d

--- mean-app/server/routes/thingapi.js
+++ mean-app/server/routes/thingapi.js
@@ -30,9 +30,8 @@ var parseTelegram = function(data, callback){
   //}
 
   //TOE1
-  regx = /1-0:1\.8\.1\((\d{5,6}\.\d{3})\*kWh\)/g
-  //found = str.match(regx);
-  found = regx.exec(data);
+  var regdx = /1-0:1\.8\.1\((\d{5,6}\.\d{3})\*kWh\)/g
+  found = str.match(regdx);
 
   console.log("found:");
   console.log(found[0]);
