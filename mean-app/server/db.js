75f82423194027d32f9156f6492b2ac1a2a3819a
kennyboy55 <kennyboy@live.nl>
2017-04-10 13:28:38 +0200

    Timezone fix and query order

--- mean-app/server/db.js
+++ mean-app/server/db.js
@@ -3,7 +3,8 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'asdf1239',
-  database : 'smarthome'
+  database : 'smarthome',
+  timezone : 'utc'
 });
 
 connection.connect(function(err) {
